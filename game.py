be05c719044667d605b84f72e610ce2a0c0710f5
Illya Naumov <illya.naumov@gmail.com>
2021-02-06 23:43:01 -0800

    linting

--- game.py
+++ game.py
@@ -1,24 +1,24 @@
 from random import randint
 
 def user_input():
-	
+    
     user_choice = 0
     while not (user_choice) in range(1,4):
         try:
             user_choice = int(input("Please enter your choice (1 - 3) : "))
         except ValueError:
-			print("Incorrect choice...")
+            print("Incorrect choice...")
     return user_choice - 1
 
 ## user_input asks user for input (1-3), and returns their selection
 
 def convert_rps(user_input: int) -> str: 
-	
+    
     switcher={
         0:'ROCK',
         1:'PAPER',
         2:'SCISSORS'
-	}
+    }
     return switcher.get(user_input, "Invalid Choice")
 
 ## convert_rps takes int as input and returns rock, paper, or scissors
@@ -34,7 +34,7 @@ def check_win(user_num: int, computer_num: int) -> str:
     result_table = [[2, 0, 1],
                     [1, 2, 0],
                     [0, 1, 2]]
-    results = ["Lost", "Won", "Tied"]         	    
+    results = ["Lost", "Won", "Tied"]
     return results[result_table[user_num][computer_num]]
 
 ## check_win() takes user_num and computer_num as inputs and returns the game result
@@ -54,32 +54,32 @@ def rps_instructions():
 
 def intro():
 
-	print("=========================================")
-	print("==     Welcome to ROCK                 ==")
-	print("==                  PAPER              ==")
-	print("==                    SCISSORS         ==")
-	print("==   COVID SELF EDUCATION EPISODE 1    ==")
-	print("==                                     ==")
-	print("==          Implemented by             ==")
-	print("==           ILLYA NAUMOV              ==")
-	print("=========================================")
+    print("=========================================")
+    print("==     Welcome to ROCK                 ==")
+    print("==                  PAPER              ==")
+    print("==                    SCISSORS         ==")
+    print("==   COVID SELF EDUCATION EPISODE 1    ==")
+    print("==                                     ==")
+    print("==          Implemented by             ==")
+    print("==           ILLYA NAUMOV              ==")
+    print("=========================================")
 
 
 def game():
 
-	user_choice = user_input()
-	computer_choice = computer_selection()
+    user_choice = user_input()
+    computer_choice = computer_selection()
 
 
-	print('You have selected {}.'.format(convert_rps(user_choice)))
-	print('The computer has selected {}.'.format(convert_rps(computer_choice)))
-	print('You have {}.'.format(check_win(user_choice, computer_choice)))
-	print()
-	if input('Type \'n\' to play again! ') in 'n':
-		game()
+    print('You have selected {}.'.format(convert_rps(user_choice)))
+    print('The computer has selected {}.'.format(convert_rps(computer_choice)))
+    print('You have {}.'.format(check_win(user_choice, computer_choice)))
+    print()
+    if input('Type \'n\' to play again! ') in 'n':
+        game()
 
 
 
 intro()
 rps_instructions()
-game()	
+game()    
