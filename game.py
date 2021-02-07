1390b0ed8fdba2c7b3ae22685fe1768a5089731d
Illya Naumov <illya.naumov@gmail.com>
2021-02-06 23:41:59 -0800

    linting

--- game.py
+++ game.py
@@ -2,37 +2,38 @@ from random import randint
 
 def user_input():
 	
-	user_choice = 0
-	while not (user_choice) in range(1,4):
-		try:
-			user_choice = int(input("Please enter your choice (1 - 3) : "))
-		except ValueError:
+    user_choice = 0
+    while not (user_choice) in range(1,4):
+        try:
+            user_choice = int(input("Please enter your choice (1 - 3) : "))
+        except ValueError:
 			print("Incorrect choice...")
-	return user_choice - 1
+    return user_choice - 1
 
 ## user_input asks user for input (1-3), and returns their selection
 
 def convert_rps(user_input: int) -> str: 
 	
-	switcher={
-		0:'ROCK',
-		1:'PAPER',
-		2:'SCISSORS'
+    switcher={
+        0:'ROCK',
+        1:'PAPER',
+        2:'SCISSORS'
 	}
-	return switcher.get(user_input, "Invalid Choice")
+    return switcher.get(user_input, "Invalid Choice")
 
 ## convert_rps takes int as input and returns rock, paper, or scissors
 
 def computer_selection() -> int:
 
-	return randint(0,2)
+    return randint(0,2)
 
 ## conputer_selection uses randint from the random library to return a choice for the opponent 
 
 def check_win(user_num: int, computer_num: int) -> str:
+    
     result_table = [[2, 0, 1],
-             	    [1, 2, 0],
-             	    [0, 1, 2]]
+                    [1, 2, 0],
+                    [0, 1, 2]]
     results = ["Lost", "Won", "Tied"]         	    
     return results[result_table[user_num][computer_num]]
 
